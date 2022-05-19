87ae5bfd87eae2983324f2a82f957eeeaa5da671
kyw04 <85551017+kyw04@users.noreply.github.com>
2022-05-19 15:36:33 +0900

    코드 간소화

--- 소스.cpp
+++ 소스.cpp
@@ -4,43 +4,20 @@
 #define C 10
 using namespace std;
 
-string dp(int n, int a, int b, int c)
-{
-	if (n >= A)
-		return dp(n % A, n / A, b, c);
-	if (n >= B)
-		return dp(n % B, a, n / B, c);
-	if (n >= C)
-		return dp(n % C, a, b, n / C);
-	if (n == 0)
-	{
-		string t = "";
-		t.push_back(a);
-		t.push_back(-1);
-		t.push_back(b);
-		t.push_back(-1);
-		t.push_back(c);
-		return t;
-	}
-	else
-		return "-1";
-}
 int main()
 {
 	int t;
 	cin >> t;
-	string result = dp(t, 0, 0, 0);
-	if (result == "-1")
-		cout << result;
+
+	int a = t / A;
+	t %= A;
+	int b = t / B;
+	t %= B;
+	int c = t / C;
+	t %= C;
+	if (t)
+		cout << -1;
 	else
-	{
-		for (int i = 0; i < result.length(); i++)
-		{
-			if (result[i] != -1)
-				cout << (int)result[i];
-			else
-				cout << ' ';
-		}
-	}
+		cout << a << ' ' << b << ' ' << c;
 	return 0;
 }
